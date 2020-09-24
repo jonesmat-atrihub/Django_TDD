8e2f2bb03e3c68d0e8e560b1966b5494f3033ff7
Matthew Jones <jonesmat@atrihub.io>
2020-09-24 15:25:31 -0700

    feat (MJ TDD): complete Chapter 3
    Completed Chapter 3 of the TDD book
    
    [ICTRAINING-478]

--- functional_tests.py
+++ functional_tests.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 import unittest
 
 
-class NewVisitorTest( unittest.TestCase ):
+class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
 
@@ -10,10 +10,10 @@ class NewVisitorTest( unittest.TestCase ):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get( 'http://localhost:8000' )
+        self.browser.get('http://localhost:8000')
 
-        self.assertIn( 'To-Do', self.browser.title )
-        self.fail( 'Finish the test!' )
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
 
 
 if __name__ == '__main__':
