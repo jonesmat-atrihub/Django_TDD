aaf3973e88953ae7a29658222223a4c17ee44bc8
Matthew Jones <jonesmat@atrihub.io>
2020-09-25 08:58:33 -0700

    feat (MJ TDD): complete Chapter 4
    Completed Chapter 4 of the TDD book
    
    [ICTRAINING-479]

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,9 @@
+import time
+
 from selenium import webdriver
 import unittest
+
+from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -12,7 +16,34 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
 
+        # User notices page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # User is invited to enter to-do item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # User types "Buy peacock feathers"
+        inputbox.send_keys('Buy peacock feathers')
+
+        # User presses "Enter", page updates and now displays:
+        # "1: Buy peacock feathers" as an item in to-do list table
+
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            'New to-do item did not appear in table'
+        )
+
         self.fail('Finish the test!')
 
 
