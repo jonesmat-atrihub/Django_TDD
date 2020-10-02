c6003ff27a053d8a76963b578f5fd6451c4fdce7
Matthew Jones <jonesmat@atrihub.io>
2020-10-02 07:20:33 -0700

    Fix new

--- superlists/settings.py
+++ superlists/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'superlists.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_tutorial',
+        'USER': 'jonesmat',
+        'PASSWORD': 'password123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
