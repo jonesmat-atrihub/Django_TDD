6cfec45af86e893fd9a8d54177bda6cef8c5c953
Matthew Jones <jonesmat@atrihub.io>
2020-10-05 09:28:50 -0700

    Fix import

--- superlists/settings.py
+++ superlists/settings.py
@@ -12,11 +12,6 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
-if 'HEROKU_APP' in os.environ:
-    # Activate Django-Heroku
-    import django_heroku
-    django_heroku.settings(locals())
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -127,3 +122,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+if 'HEROKU_APP' in os.environ:
+    # Activate Django-Heroku
+    import django_heroku
+    django_heroku.settings(locals())
+    
